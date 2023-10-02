4afef1e907cd547aecfb5e149339c1a1f3020b8e
MartinaSTV <foldermate@hotmail.com>
2023-10-02 14:18:52 +0200

    component struktur

---
+++ src/Components/RecomendedMovies.jsx
@@ -0,0 +1,5 @@
+function RecomendedMovies() {
+  return <section>RecomendedMoviess</section>;
+}
+
+export default RecomendedMovies;
---
+++ src/Views/Home.jsx
@@ -0,0 +1,12 @@
+import RecomendedMovies from "../Components/RecomendedMovies";
+
+function Home() {
+  return (
+    <main>
+      <h1>Notflix</h1>
+      <RecomendedMovies />
+    </main>
+  );
+}
+
+export default Home;
