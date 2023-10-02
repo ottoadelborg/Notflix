bbfbd673f9f971cb3c833da570f7d668219c45cc
MartinaSTV <foldermate@hotmail.com>
2023-10-02 15:20:17 +0200

    Random movies

--- src/Components/RecomendedMovies.jsx
+++ src/Components/RecomendedMovies.jsx
@@ -1,5 +1,27 @@
+import movies from "../assets/movies.json";
+
+// ta fram 5 random movies
+// gör en karusell med filmer bild och pilar.
+
 function RecomendedMovies() {
-  return <section>RecomendedMoviess</section>;
+  const RandomMovies = [];
+  console.log(RandomMovies);
+
+  for (let i = 0; i < 5; i++) {
+    const RandomMovie = movies[Math.floor(Math.random() * movies.length)];
+    RandomMovies.push(RandomMovie);
+  }
+
+  return (
+    <section>
+      <h1>Recommended Movies</h1>
+      <article>
+        {RandomMovies.map((movie, index) => (
+          <img key={index} src={movie.thumbnail} alt="" />
+        ))}
+      </article>
+    </section>
+  );
 }
 
 export default RecomendedMovies;
---
+++ src/Tests/RekommendedMovies.test.jsx
@@ -0,0 +1,13 @@
+import { render, screen } from "@testing-library/react";
+import { describe, it, expect } from "vitest";
+import RecomendedMovies from "../Components/RecomendedMovies";
+
+describe("Recommended Movies", () => {
+  it("should display header", () => {
+    render(<RecomendedMovies />);
+    expect(screen.getByText("Recommended Movies")).toBeInTheDocument();
+  });
+  it("should display Random Movies", () => {
+    render(<RecomendedMovies />);
+  });
+});
