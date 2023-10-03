a66d97ead6f66f235ff88ae55c3a9f5077221443
JavDaGreat <1998javad@gmail.com>
2023-10-03 11:00:57 +0200

    creating movie-List

--- src/Views/Categories.jsx
+++ src/Views/Categories.jsx
@@ -1,9 +1,31 @@
 import Navbar from "../Components/Navbar";
+import { useState } from "react";
+import CategoryButtons from "../Components/CategoryButtons";
+import movies from "../assets/movies.json";
+import "../Style/Categories.scss";
+
 function Categories() {
+  const [filmCategory, setFilmCategory] = useState("Drama");
+
+  const test = movies?.map((movie) => {
+    if (movie.genre.includes(filmCategory)) {
+      return (
+        <div className="movie-show">
+          <img src={movie.thumbnail} alt="No img exist" />
+          <p> {movie.title}</p>
+        </div>
+      );
+    }
+  });
+
   return (
-    <>
+    <div className="category-page">
       <Navbar />
-    </>
+
+      <CategoryButtons />
+
+      <div className="movie-list">{test}</div>
+    </div>
   );
 }
 
