5bec414c4908935d22d0258743183bcaed90ac6e
JavDaGreat <1998javad@gmail.com>
2023-10-03 11:17:56 +0200

    Styling and import Nav

--- src/Views/Categories.jsx
+++ src/Views/Categories.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import CategoryButtons from "../Components/CategoryButtons";
 import movies from "../assets/movies.json";
 import "../Style/Categories.scss";
+import Navbar from "../Components/Navbar";
 
 function Categories() {
   const [filmCategory, setFilmCategory] = useState("Drama");
@@ -19,6 +20,8 @@ function Categories() {
 
   return (
     <div className="category-page">
+      <Navbar />
+
       <CategoryButtons />
 
       <div className="movie-list">{test}</div>
