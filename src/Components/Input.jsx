14fa10d76652c11957b0b4bb693e21c3036e49fe
Otto Adelborg <otto.adelborg@gmail.com>
2023-10-03 14:51:48 +0200

    Update

--- src/Components/Input.jsx
+++ src/Components/Input.jsx
@@ -1,20 +1,46 @@
 import { FaSearch } from "react-icons/fa";
+import movies from "../assets/movies.json";
+import { useState } from "react";
 import "../Style/Input.scss";
 
-function MovieInput({ setSearchMovie, handleClick }) {
+function MovieInput() {
+  const [searchMovie, setSearchMovie] = useState("");
+  const [data, setData] = useState("");
+
+  const content = movies.filter(function (movie) {
+    return movie.title === searchMovie;
+  });
+
+  const handleClick = () => {
+    setData(content);
+  };
+  console.log(data);
+
   return (
-    <div>
-      <input
-        type="text"
-        placeholder="Search movie..."
-        onChange={(e) => {
-          setSearchMovie(e.target.value);
-        }}
-        className="search--movie__input"
-      />
-      <button className="search--movie__button">
-        <FaSearch className="search--button" onClick={handleClick} />
-      </button>
+    <div className="search--container">
+      <div className="input--container">
+        <input
+          type="text"
+          placeholder="Search movie..."
+          onChange={(e) => {
+            setSearchMovie(e.target.value);
+          }}
+          className="search--movie__input"
+        />
+        <button className="search--movie__button" onClick={handleClick}>
+          <FaSearch className="search--button" />
+        </button>
+      </div>
+      {data[0]?.title ? (
+        <div className="movie-show">
+          <img
+            src={data[0].thumbnail}
+            alt="Hittar ingen bild"
+            className="movie--card"
+          />
+          <p className="movie--title">{data[0].title}</p>
+        </div>
+      ) : null}
     </div>
   );
 }
--- src/Views/Home.jsx
+++ src/Views/Home.jsx
@@ -1,31 +1,15 @@
 import Input from "../Components/Input";
-import ShowHomeMovie from "../Components/ShowHomeMovie";
-import movies from "../assets/movies.json";
-import "../Style/Input.scss";
-import { useState } from "react";
-import RecomendedMovies from "../Components/RecomendedMovies";
 import Navbar from "../Components/Navbar";
+import RecomendedMovies from "../Components/RecomendedMovies";
+import "../Style/Input.scss";
 import "../Style/Home.scss";
 
 function Home() {
-  const [searchMovie, setSearchMovie] = useState("");
-  const [data, setData] = useState("");
-
-  const content = movies.filter(function (movie) {
-    return movie.title === searchMovie;
-  });
-
-  const handleClick = () => {
-    setData(content);
-  };
-  console.log(data);
-
   return (
     <div className="search--movie__container">
       <Navbar />
-      <Input setSearchMovie={setSearchMovie} handleClick={handleClick} />
+      <Input />
       <RecomendedMovies />
-      <ShowHomeMovie title={data[0]?.title} thumbnail={data[0]?.thumbnail} />
     </div>
   );
 }
