27d24d3b17351ebfe6fa0b0784592cd8de4909d4
MartinaSTV <foldermate@hotmail.com>
2023-10-03 11:03:07 +0200

    removed nor working slider

--- src/Views/Home.jsx
+++ src/Views/Home.jsx
@@ -3,7 +3,6 @@ import Navbar from "../Components/Navbar";
 import "../Style/Input.scss";
 import Recommended from "../Components/Recommended";
 import "../Style/Home.scss";
-
 function Home() {
   return (
     <div className="search--movie__container">
