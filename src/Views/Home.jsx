0a51da2e7d6e42b088fa70bd52db614dd6985efb
Otto Adelborg <otto.adelborg@gmail.com>
2023-10-03 13:00:30 +0200

    Added functionality to input+button

--- src/Views/Home.jsx
+++ src/Views/Home.jsx
@@ -1,5 +1,28 @@
+import Input from "../Components/Input";
+import ShowHomeMovie from "../Components/ShowHomeMovie";
+import movies from "../assets/movies.json";
+import "../Style/Input.scss";
+import { useState } from "react";
+
 function Home() {
-  return <p>hej</p>;
+  const [searchMovie, setSearchMovie] = useState("");
+  const [data, setData] = useState("");
+
+  const content = movies.filter(function (movie) {
+    return movie.title === searchMovie;
+  });
+
+  const handleClick = () => {
+    setData(content);
+  };
+  console.log(data);
+
+  return (
+    <div className="search--movie__container">
+      <Input setSearchMovie={setSearchMovie} handleClick={handleClick} />
+      <ShowHomeMovie title={data[0]?.title} thumbnail={data[0]?.thumbnail} />
+    </div>
+  );
 }
 
 export default Home;
