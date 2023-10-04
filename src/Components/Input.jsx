48beea46c6dee26f54c90b6893e63ac4e1d3bdb4
Otto Adelborg <otto.adelborg@gmail.com>
2023-10-04 10:26:30 +0200

    navigate

--- src/Components/Input.jsx
+++ src/Components/Input.jsx
@@ -1,11 +1,14 @@
 import { FaSearch } from "react-icons/fa";
 import movies from "../assets/movies.json";
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import "../Style/Input.scss";
 
 function MovieInput() {
   const [searchMovie, setSearchMovie] = useState("");
   const [data, setData] = useState("");
+
+  const navigate = useNavigate();
 
   const content = movies.filter(function (movie) {
     return movie.title === searchMovie;
@@ -32,7 +35,12 @@ function MovieInput() {
         </button>
       </div>
       {data[0]?.title ? (
-        <div className="movie-show">
+        <div
+          className="movie-show"
+          onClick={() => {
+            navigate("/Notflix/film-view", { state: { movie: data[0] } });
+          }}
+        >
           <img
             src={data[0].thumbnail}
             alt="Hittar ingen bild"
