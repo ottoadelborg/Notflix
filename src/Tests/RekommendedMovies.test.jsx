417ff32427b31f853646af1f8826636abd6d00e5
MartinaSTV <foldermate@hotmail.com>
2023-10-04 15:08:02 +0200

    test

--- src/Tests/RekommendedMovies.test.jsx
+++ src/Tests/RekommendedMovies.test.jsx
@@ -3,6 +3,7 @@ import { describe, it, expect } from "vitest";
 import Recommended from "../Components/Recommended";
 import { BrowserRouter } from "react-router-dom";
 import userEvent from "@testing-library/user-event";
+//test
 
 describe("Recommended Movies", () => {
   it("should display header", () => {
