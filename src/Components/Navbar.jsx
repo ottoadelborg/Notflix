659a60a3c50e80fbbaa6704b8b5d1bf836574b01
Otto Adelborg <otto.adelborg@gmail.com>
2023-10-04 15:06:13 +0200

    logo

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,9 +1,11 @@
 import "../Style/Navbar.scss";
+import logo from "../assets/download.png";
 import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
     <section className="navbar">
+      <img src={logo} alt="Logo" className="Logo" />
       <Link className="navbar__links" to={"../Notflix"}>
         Home
       </Link>
