8c215de20698cb0bd8d1c1a3bb18ba3e4315cafa
JavDaGreat <1998javad@gmail.com>
2023-10-09 10:46:42 +0200

    Add placeHolderImg, Add bookmark ,test for bookmrk

--- src/Tests/Category-page.test.jsx
+++ src/Tests/Category-page.test.jsx
@@ -4,6 +4,7 @@ import Categories from "../Views/Categories";
 import { BrowserRouter } from "react-router-dom";
 import userEvent from "@testing-library/user-event";
 import FilmView from "../Views/Film-view";
+import Bookmarks from "../Views/Bookmarks";
 
 describe("Categories-Page", () => {
   it("should display Categorie", async () => {
@@ -21,11 +22,37 @@ describe("Categories-Page", () => {
     expect(await screen.findByText("The Dark Knight"));
     await userEvent.click(film);
     expect(await window.location.pathname).toBe("/Notflix/film-view");
+
     render(
       <BrowserRouter>
         <FilmView />
       </BrowserRouter>
     );
+
     expect(await screen.getByText("PG-13")).toBeInTheDocument();
+  });
+  it("should display bookmark", async () => {
+    render(
+      <BrowserRouter>
+        <Categories />
+      </BrowserRouter>
+    );
+
+    const BookmarkBTN = await screen.getAllByRole("button", {
+      name: /Add to Bookmark/i,
+    });
+    const firstBookmarkButton = BookmarkBTN.find((button) =>
+      /Add to Bookmark/i.test(button.textContent)
+    );
+    await userEvent.click(firstBookmarkButton);
+    render(
+      <BrowserRouter>
+        <Bookmarks />
+      </BrowserRouter>
+    );
+
+    expect(await screen.getByText("Remove")).toBeInTheDocument();
+
+    screen.debug();
   });
 });
