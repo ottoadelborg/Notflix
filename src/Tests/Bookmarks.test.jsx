5a8a97dcca98f3cc06bda57af49a7829b9c87e03
plindstromm <mukkepp@gmail.com>
2023-10-09 12:52:42 +0200

    cleanup bookmarks

--- src/Tests/Bookmarks.test.jsx
+++ src/Tests/Bookmarks.test.jsx
@@ -1,11 +1,10 @@
-import { fireEvent, render, screen, waitFor } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import FilmView from "../Views/Film-view";
 import Categories from "../Views/Categories";
 import { BrowserRouter } from "react-router-dom";
 import { describe, it, expect } from "vitest";
 import Bookmarks from "../Views/Bookmarks";
-import { MemoryRouter, Route, Link } from "react-router-dom";
 
 describe("navigate to film-view", () => {
   it("should navigate from categories to film-view, and click add to favourite, then render the selected movie", async () => {
