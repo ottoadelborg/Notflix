2ee6175a23cfbe25df636f4dbde3f8a7156d8605
JavDaGreat <1998javad@gmail.com>
2023-10-09 10:46:42 +0200

    Add placeHolderImg, Add bookmark ,test for bookmrk

--- src/Views/Categories.jsx
+++ src/Views/Categories.jsx
@@ -9,6 +9,9 @@ function Categories() {
   const [genre, setGenre] = useState([]);
   const navigate = useNavigate();
 
+  const placeholderImg =
+    "https://placehold.jp/30/3d4070/ffffff/380x562.png?text=No%20image";
+
   useEffect(() => {
     const genreArray = [];
 
@@ -25,6 +28,12 @@ function Categories() {
     setGenre(genreArray);
   }, []);
 
+  const saveToStorage = (movie) => {
+    const savedMovies = JSON.parse(localStorage.getItem("savedMovies")) || [];
+    savedMovies.push(movie);
+    localStorage.setItem("savedMovies", JSON.stringify(savedMovies));
+  };
+
   const test = genre.map((gen, index) => {
     return (
       <button
@@ -39,14 +48,31 @@ function Categories() {
   const content = movies?.map((movie, idx) => {
     if (movie.genre.includes(filmCategory)) {
       return (
-        <div
-          key={idx}
-          className="movie-show"
-          onClick={() => {
-            navigate("/Notflix/film-view", { state: { movie } });
-          }}>
-          <img src={movie.thumbnail} alt="No img exist" />
-          <p> {movie.title}</p>
+        <div key={idx} className="movie-show">
+          <img
+            src={movie.thumbnail}
+            alt="No img exist"
+            onClick={() => {
+              navigate("/Notflix/film-view", { state: { movie } });
+            }}
+            onError={(e) => {
+              e.target.src = placeholderImg;
+            }}
+          />
+          <p
+            onClick={() => {
+              navigate("/Notflix/film-view", { state: { movie } });
+            }}>
+            {" "}
+            {movie.title}
+          </p>
+          <button
+            onClick={() => {
+              saveToStorage(movie);
+            }}
+            className="button">
+            Add to Bookmark
+          </button>
         </div>
       );
     }
@@ -55,8 +81,8 @@ function Categories() {
   return (
     <div className="category-page">
       <Navbar />
-      <div className="buttons">{test}</div>
 
+      <div className="buttons">{test}</div>
       <div className="movie-list">{content}</div>
     </div>
   );
