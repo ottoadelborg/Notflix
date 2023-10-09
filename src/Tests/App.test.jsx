8402e029bf7bac617e045d83be7aafa571703004
MartinaSTV <foldermate@hotmail.com>
2023-10-09 11:08:27 +0200

    fix test

--- src/Tests/App.test.jsx
+++ src/Tests/App.test.jsx
@@ -1,14 +1,30 @@
 import { render, screen } from "@testing-library/react";
 import { describe, it, expect } from "vitest";
 import userEvent from "@testing-library/user-event";
-import App from "../App";
+import Home from "../Views/Home";
+import { BrowserRouter } from "react-router-dom";
+import FilmView from "../Views/Film-view";
 
 describe("Test av funktion som kräver alla views", () => {
   it("should display Film-view on click Recommended movies", async () => {
-    render(<App />);
+    render(
+      <BrowserRouter>
+        <Home />
+      </BrowserRouter>
+    );
+
     const user = userEvent.setup();
     const movie = screen.getAllByRole("img");
-    await user.click(movie[0]);
-    expect(await screen.findByText("Genre:"));
+    screen.debug();
+    await user.click(movie[1]);
+    console.log(movie[0], "consolelOg ");
+
+    render(
+      <BrowserRouter>
+        <FilmView />
+      </BrowserRouter>
+    );
+    expect(window.location.pathname).toBe("/Notflix/film-view");
+    expect(screen.findByText("Genre:"));
   });
 });
